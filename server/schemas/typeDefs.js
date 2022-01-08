e983f6d62a3207a87045ed7ecf7f48fc6cfed81a
Alfonso Robles <fons3517@gmail.com>
2022-01-08 14:17:03 -0600

    added basic code for typeDefs.js file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,33 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type User {
+  _id: ID
+  firstName: String
+  lastName: String
+  email: String
+  trails: [Trail]
+}
+
+type Trail {
+  _id: ID
+  name: String
+}
+
+type Auth {
+  token: ID
+  user: User
+}
+
+type Query {
+  
+}
+
+type Mutations {
+  addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+  addTrail(name: String!): Trail
+  updateUser(firstName: String, lastName: String, email: String, password: String): User
+  login(email: String!, password; String!): Auth
+}
 `;
 module.exports = typeDefs;
