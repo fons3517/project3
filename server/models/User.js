a1c4dde715ca0e6ebb01ace153442d9308adf7df
Alfonso Robles <fons3517@gmail.com>
2022-01-08 16:00:53 -0600

    added updates to User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,43 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+const bcrypt = require('bcrypt');
+const Trail = require('./Trail');
+
 
 const userSchema = new Schema({
   username: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    match: [/.+@.+\..+/, "Must use a valid email address"],
+    match: [/.+@.+\..+/, "Must use a valid email address"]
   },
   password: {
     type: String,
     required: true,
+    minLenth: 8
   },
+  trails: [Trail.schema]
 });
+
+// pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+//compare incoming password w/ hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
