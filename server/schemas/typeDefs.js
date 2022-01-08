f0ea1fc9eea7327f91af184ae07680d3126666f1
Andrew Martinez <andrewmartinez6313@gmail.com>
2022-01-08 14:22:53 -0600

    added code to models folder

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,13 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+  type Query {
+    me: User
+  }
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+  }
 `;
 module.exports = typeDefs;
