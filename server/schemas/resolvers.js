b6fc72d8c74da44d4bc678856a8a015cdc93db99
Alfonso Robles <fons3517@gmail.com>
2022-01-08 13:59:33 -0600

    added typeDefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,19 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { } = require('../models');
+const { signToken } = require('../utils/auth');
+
+// Set-up Stripe for future development
+// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc);
+
+const resolvers = {
+  Query: {
+    model: async () => {
+      return await model.find();
+    },
+    models: async (parent, { }) => {
+
+    }
+  }
+};
+
+module.exports = resolvers;
