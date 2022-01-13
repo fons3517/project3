aa48c505c6b368c47808ea47555518a3b5d5aed5
Alfonso Robles <fons3517@gmail.com>
2022-01-12 19:16:58 -0600

    added code inside of queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,27 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_
+export const QUERY_TRAILS = gql`
+  query getTrails {
+    trails {
+      _id
+      name
+      location
+      distance
+      difficulty
+      rating
+    }
+  }
+`;
+
+export const QUERY_SINGLE_TRAIL = gql`
+  query getSingleTrail($trailId: ID!) {
+    trail(trailId: $trailId) {
+      _id
+      name
+      locatioin
+      distance
+      difficulty
+      rating
+    }
+  }
+`;
