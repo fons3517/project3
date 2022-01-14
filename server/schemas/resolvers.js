60eaa866c2c0ded3897a7675878a79956d08f16c
Maria Vallejo <maria.vallejo29@yahoo.com>
2022-01-14 12:56:39 -0600

    fixed graphQL for app to run

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -20,7 +20,7 @@ const resolvers = {
   },
   Mutation: {
     addUser: async (parent, args) => {
-      const user = await UserInputError.create(args);
+      const user = await User.create(args);
       const token = signToken(user);
       return { token, user };
     },
