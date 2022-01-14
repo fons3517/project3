7d8ebb562538aad30726093cb8d77c4bd9813db1
Maria Vallejo <maria.vallejo29@yahoo.com>
2022-01-14 15:46:13 -0600

    updated auth for grpahQL playground to connect to server

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,20 +1,20 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 // set token .env and expiration date
 const secret = process.env.DB_SECRET;
-const expiration = '2h';
+const expiration = "2h";
 
 module.exports = {
   authMiddleware: function ({ req }) {
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(" ").pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'NO Such Token Exists Here!' });
+      return req;
     }
 
     //verifying token and getting user data out
@@ -22,12 +22,13 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('This Token is INVALID!!!');
-      return res.status(400).json({ message: 'This is an INVALID Token' });
+      console.log("Invalid token");
     }
+
+    return req;
   },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  signToken: function ({ firstName, lastName, email, _id }) {
+    const payload = { firstName, lastName, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
@@ -35,8 +36,5 @@ module.exports = {
     const check =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return check.test(String(email).toLowerCase());
-  },
+  }
 };
-
-
-
