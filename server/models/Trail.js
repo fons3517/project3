d044d4ba519107cf189184c265c4ec613daa49d8
Maria Vallejo <maria.vallejo29@yahoo.com>
2022-01-17 18:14:19 -0600

    updated trail model to fit api results

--- server/models/Trail.js
+++ server/models/Trail.js
@@ -3,7 +3,7 @@ const { Schema, model } = mongoose;
 
 const trailSchema = new Schema({
   trailId: {
-    type: String,
+    type: Number,
     required: true,
   },
   name: {
@@ -11,22 +11,12 @@ const trailSchema = new Schema({
   },
   description: {
     type: String,
-    required: true,
   },
   directions: {
     type: String,
-    required: true
-  },
-  lat: {
-    type: String,
-  },
-  lon: {
-    type: String,
-    required: true,
   },
   difficulty: {
     type: String,
-    required: true
   },
   length: {
     type: String,
@@ -34,8 +24,12 @@ const trailSchema = new Schema({
   rating: {
     type: Number,
   },
+  link: {
+    type: String
+  },
+  img: {
+    type:String
+  }
 });
 
-const Trail = mongoose.model("Trail", trailSchema)
-
-module.exports = Trail;
+module.exports = trailSchema;
