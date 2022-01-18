dacab6a8c67e0eed0fb0d0869c39581013f15b58
Alfonso Robles <fons3517@gmail.com>
2022-01-18 17:03:10 -0600

    deleted blocked out code from Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-// import React, { useState, useEffect } from "react";
-// import {
-//   Jumbotron,
-//   Container,
-//   Col,
-//   Form,
-//   Button,
-//   Card,
-//   CardColumns
-// } from "react-bootstrap";
-// import { locationApi } from "../utils/API";
-// import { searchTrailApi } from "../utils/API";
-// import Auth from "../utils/auth";
-// import { saveTrailIds, getSavedTrailIds } from "../utils/localStorage";
-// import { useMutation } from "@apollo/client";
-// import { ADD_HIKE } from "../utils/mutations";
-// import { SAVE_TRAIL } from "../utils/mutations";
-
-
-=======
 import React from "react";
 import NavBar from "../components/navbar/NavBar";
 import Footer from "../components/footer/Footer";
@@ -36,4 +15,3 @@ const Home = () => {
 };
 
 export default Home;
->>>>>>> 4e47cfaabcf211bc1a5e60b3d241bfedc66a8b57
