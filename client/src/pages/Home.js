addeeb33adfd79c48176719b3fbd8237b476b05e
Alfonso Robles <fons3517@gmail.com>
2022-01-18 16:14:52 -0600

    commiting updates

---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+// import React, { useState, useEffect } from "react";
+// import {
+//   Jumbotron,
+//   Container,
+//   Col,
+//   Form,
+//   Button,
+//   Card,
+//   CardColumns
+// } from "react-bootstrap";
+// import { locationApi } from "../utils/API";
+// import { searchTrailApi } from "../utils/API";
+// import Auth from "../utils/auth";
+// import { saveTrailIds, getSavedTrailIds } from "../utils/localStorage";
+// import { useMutation } from "@apollo/client";
+// import { ADD_HIKE } from "../utils/mutations";
+// import { SAVE_TRAIL } from "../utils/mutations";
+
+
